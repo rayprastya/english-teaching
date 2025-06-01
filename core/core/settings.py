a85672940c2673286e94a47aecbb8e30eb41259f
rayprastya <rayhan.prastya@gmail.com>
2025-06-01 20:44:01 +0700

    use constant in the settings set up

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import core.utils.constant as constant
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': constant.DB_NAME,
+        'USER': constant.DB_USER,
+        'PASSWORD': constant.DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': constant.DB_PORT,
     }
 }
 
