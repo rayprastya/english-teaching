b5eb52dd902fe09167024fccb48b08a501d8db1a
rayprastya <rayhan.prastya@gmail.com>
2025-06-02 01:29:31 +0700

    fix: use cbv message view instead of fbv

--- core/teaching/urls.py
+++ core/teaching/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RoomView, send_message
+from .views import RoomView, MessageView
 
 urlpatterns = [
     path('room/', RoomView.as_view(), name='room'),
     path('room/<int:room_id>/', RoomView.as_view(), name='room_detail'),
-    path('room/<int:room_id>/send/', send_message, name='send_message'),
+    path('room/<int:room_id>/send/', MessageView.as_view(), name='send_message'),
 ]
 
